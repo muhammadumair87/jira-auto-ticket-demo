370e0aeada8f6112bd768207fec68bd54d1cfe14
Muhammad Umair <110074959+muhammadumair87@users.noreply.github.com>
2026-02-23 16:43:55 +0100

    Update security_scan.py

--- security_scan.py
+++ security_scan.py
@@ -1,15 +1,8 @@
 import sys
-import random
 
 def run_scan():
-    vulnerability_detected = random.choice([True, False])
-
-    if vulnerability_detected:
-        print("CRITICAL: Vulnerability detected!")
-        sys.exit(1)  # Fails pipeline
-    else:
-        print("Scan clean.")
-        sys.exit(0)
+    print("CRITICAL: Vulnerability detected!")
+    sys.exit(1)
 
 if __name__ == "__main__":
     run_scan()
